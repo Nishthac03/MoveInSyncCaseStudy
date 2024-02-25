b53f94b5cfd78e9b92fbc8ef511dbe3d3787c358
Nishtha Chopra <nishthac3@gmail.com>
2024-02-25 12:23:52 +0530

    Update AutoLogin.py

--- AutoLogin.py
+++ AutoLogin.py
@@ -22,10 +22,12 @@ driver.get("https://demoqa.com/login")
 username_field = WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.XPATH, "//input[@id='username']"))
 )
+username_field.send_keys(nishthac3)
 
 password_field = WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.XPATH, "//input[@id='password']"))
 )
+password_field.send_keys(Nishtha@c3)
 
 submit_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, "input[type=\"submit\"]"))
